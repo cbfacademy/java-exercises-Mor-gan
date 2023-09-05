7ed5ee6ec90b38778d4ede1fc4b5e5423c1c0951
Mor-gan <ogundipeabimbola9@gmail.com>
2023-09-05 18:06:18 +0100

    Input file - write file

--- exercises/input-output/src/main/java/com/cbfacademy/App.java
+++ exercises/input-output/src/main/java/com/cbfacademy/App.java
@@ -1,7 +1,22 @@
 package com.cbfacademy;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+        String filePath = "C:\\cbfacademy\\java-exercises-Mor-gan\\exercises\\input-output\\src\\main\\resources\\exercise.txt";
+        try (FileReader fileReader = new FileReader(filePath);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+                
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.println("An error occurred while reading the file: " + e.getMessage());
+        }
+}
+
 }
--- exercises/java-exceptions/src/main/java/com/cbfacademy/FileExtension.java
+++ exercises/java-exceptions/src/main/java/com/cbfacademy/FileExtension.java
@@ -7,8 +7,7 @@ public class FileExtension{
     public int check(String filename) throws InvalidFilenameException {
         if (fileName == null || fileName.isEmpty()) {
             throw new InvalidFilenameException("Invalid filename: " + fileName);
-        }
-    
+        }    
       if (this.fileName.endsWith(".java")) {
         return 1;
       }else{
